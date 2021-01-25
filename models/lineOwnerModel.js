327863791ae300767f46ca27f6b39c4d9311d1cd
JakeRSA <jakenudels@gmail.com>
2021-01-25 12:40:18 +0200

    added db methods to line owner model

--- models/lineOwnerModel.js
+++ models/lineOwnerModel.js
@@ -1,4 +1,5 @@
 const mongoUtil = require("../utils/dbConnection");
+const { ObjectID } = require("mongodb");
 
 module.exports = class LineOwner {
   constructor() {
@@ -11,6 +12,77 @@ module.exports = class LineOwner {
       );
       const newOwner = newOwnerCursor.ops[0];
       return newOwner;
+    } catch (err) {
+      return err.stack;
+    }
+  };
+  getLineOwnerByEmail = async (email) => {
+    try {
+      const lineOwner = await this.lineOwnersCollection.findOne({ email });
+      return lineOwner;
+    } catch (err) {
+      return err.stack;
+    }
+  };
+  getLineOwnerById = async (id) => {
+    try {
+      const lineOwner = await this.lineOwnersCollection.findOne({
+        _id: ObjectID(id),
+      });
+      return lineOwner;
+    } catch (err) {
+      return err.stack;
+    }
+  };
+  changeLineOwnerSettings = async (accountSettings) => {
+    /**
+     * @param {Object} accountSettings - Changes to user settings
+     * @param {string} accountSettings.id - User id
+     * @param {string} [accountSettings.email] - User's new email address
+     * @param {string} [accountSettings.displayName] - User's new display name
+     */
+
+    const filter = { _id: ObjectID(id) };
+    let updateDoc = {
+      $set: {},
+    };
+    if (accountSettings.email) {
+      const existingUser = await this.getLineOwnerByEmail(
+        accountSettings.email
+      );
+      if (existingUser) throw new Error("Email address is taken");
+      else updateDoc.$set.email = accountSettings.email;
+    }
+    if (accountSettings.displayName)
+      updateDoc.$set.displayName = accountSettings.displayName;
+
+    try {
+      await this.lineOwnersCollection.findOneAndUpdate(filter, updateDoc);
+    } catch (err) {
+      return err.stack;
+    }
+  };
+  changeLineOwnerPassword = async (updateObject) => {
+    /**
+     * @param {Object} updateObject - Changes to user settings
+     * @param {string} updateObject.id
+     * @param {string} updateObject.oldPassword
+     * @param {string} updateObject.newPassword
+     */
+
+    const filter = { _id: ObjectID(updateObject.id) };
+    // verify old password
+    // hash password
+    let hashedPassword;
+    //
+
+    const updateDoc = {
+      $set: {
+        password: hashedPassword,
+      },
+    };
+    try {
+      await this.lineOwnersCollection.findOneAndUpdate(filter, updateDoc);
     } catch (err) {
       return err.stack;
     }
