ac2014ee532fc61daf322565028a10ff280627b3
Aaron Lieblich <lieblichaaron@gmail.com>
2021-01-25 16:54:10 +0200

    add get line route

--- routes/lineRouter.js
+++ routes/lineRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { addNewLine } = require("../controllers/lineCtrlr");
+const { addNewLine, getLineById } = require("../controllers/lineCtrlr");
 const {} = require("../controllers/validator");
 
 router.post("", addNewLine);
+
+router.get("/:id", getLineById);
 
 module.exports = router;
