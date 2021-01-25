094027229868e37f5028f3c3939e320133af383a
JakeRSA <jakenudels@gmail.com>
2021-01-25 14:56:41 +0200

    added get current user route

--- routes/lineOwnerRouter.js
+++ routes/lineOwnerRouter.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const {
   addNewLineOwner,
   loginLineOwner,
+  getLoggedInUser,
 } = require("../controllers/lineOwnerCtrlr");
 
 const {} = require("../controllers/validator");
 
 router.post("/", addNewLineOwner);
 router.post("/", loginLineOwner);
+router.get("/current-user", getLoggedInUser);
 
 module.exports = router;
